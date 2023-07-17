7ad701fe9285eea39790b87e4fba85b19c5f1841
Bharath Bairy <139691118+akaBharath@users.noreply.github.com>
2023-07-17 13:31:48 +0530

    Update BMI.java

--- src/BMI.java
+++ src/BMI.java
@@ -1,19 +1,28 @@
+/*
+Author: Bharath Bairy (instagram: @akabharathbairy)
+Date modified: 17-07-2023
+Time modified: 01:31 PM GMT+5:30
+*/
+
 public class BMI {
     private int weight;
     private double height;
-    public BMI(int weight, double height){
+    
+    public BMI(int weight, double height) {
         this.weight = weight;
+        
         if(height > 100) { // For best user experience.
             this.height = height/100;
         } else {
             this.height = height;
         }
     }
-    public double getBMI(){
+    
+    public double getBMI() {
         return weight/(height*height);
     }
     
-    public String getStatus(){
+    public String getStatus() {
         if (getBMI() >= 0 && getBMI() <= 18.4) {
             return "underweight";
         } else if (getBMI() > 18.4 && getBMI() <= 25.4) {
